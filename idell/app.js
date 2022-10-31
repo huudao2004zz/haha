40733e3e7cbb0f3eb41455f0767a282a27dca546
huudao2004zz <111514495+huudao2004zz@users.noreply.github.com>
2022-10-31 15:43:24 +0700

    Add files via upload

---
+++ idell/app.js
@@ -0,0 +1,32 @@
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope) {
+    console.log("My controller.....");
+    $scope.users = [
+        {name: "shang", email: "shaangchi@gmial.com", fullname: "haha"},
+        {name: "Hieu", email: "hie@gmial.com", fullname: "hieu tran"},
+        {name: "anh hai", email: "hehe@gmial.com", fullname: "anh hai"}
+
+    ];
+    $scope.addUser = function () {
+        $scope.users.push($scope.newUser);
+        $scope.newUser = {};
+        $scope.message = "New User Added successfully";
+
+    };
+   $scope.selectUser = function(user){
+        console.log(user);
+        $scope.clickedUser = user ;
+
+    };
+    $scope.editUser =function (){
+        $scope.message="User Edited successfully";
+};
+    $scope.deleteUser =function (){
+        $scope.users.splice($scope.users.indexOf($scope.clickedUser));
+        $scope.message= "User Deleted successfully";
+
+    };
+    $scope.clearMessage =function (){
+        $scope.message="";
+    };
+});
